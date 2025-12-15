c26fb69555cd7820836b99721575326696432165
rahaf <irahafdev@icloud.com>
2025-12-15 18:25:09 +0300

    Added main and dispalyed all info there

--- Animatronic.java
+++ Animatronic.java
@@ -25,5 +25,26 @@ public class Animatronic{
             System.out.println(name + " is idling.");
         }
     }
+    public void DisplayInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Type: " + type);
+        System.out.println("Location: " + location);
+        System.out.println("Agression Level: " + AgressionLevel);
+        System.out.println("Active: " + isActive);
+    }
+    public static void main(String[] args) {
+        Animatronic animatronic1 = new Animatronic("Freddy", "Bear", "Stage", 8, true);
+        Animatronic animatronic2 = new Animatronic("Bonnie", "Rabbit", "Backstage", 5, true);
+        Animatronic animatronic3 = new Animatronic("Chica", "Chicken", "Kitchen", 3, false);
+        
+        animatronic1.DisplayInfo();
+        animatronic1.Act();
+        animatronic2.DisplayInfo();
+        animatronic2.Act();
+        animatronic3.DisplayInfo();
+        animatronic3.Act();
+    }
 
 }
+
+
