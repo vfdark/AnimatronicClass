bc1df3b62875f06c4d4258d655851113b235123c
rahaf <irahafdev@icloud.com>
2025-12-15 17:58:35 +0300

    Added File

---
+++ Animatronic.java
@@ -0,0 +1,15 @@
+public class Animatronic{
+    String name;
+    String type;
+    String location;
+    int AgressionLevel;
+    boolean isActive;
+    
+    public Animatronic(String name, String type, String location, int AgressionLevel, boolean isActive){
+        this.name = name;
+        this.type = type;
+        this.location = location;
+        this.AgressionLevel = AgressionLevel;
+        this.isActive = isActive;
+    }
+}
